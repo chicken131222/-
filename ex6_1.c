0454d5e2e2bbc38df4e42e3ab760f872cf834a42
ChickenMan <39923533+chicken131222@users.noreply.github.com>
2019-10-28 19:23:20 +0900

    Update ex6_1.c

--- ex6_1.c
+++ ex6_1.c
@@ -3,7 +3,8 @@
 
  int main(void) {
      int a;
-     a = system("ps -ef | grep han > han.txt");
+     a = system("ps -ef | grep han > han.txt");  // ps -ef | grep han > han.txt 명령 실행하도록 system함수호출 
+  //인자로 전달된 명령은 현재 실행 중인 프로세스들에서 han을 포함한 내용을 찾아 han.txt에 리다이렉션 
      printf("Return Value : %d\n", a);
      
      return 0;
