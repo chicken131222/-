8bf8a302b11d300cc3ae74a149b111e2bd24800c
ChickenMan <39923533+chicken131222@users.noreply.github.com>
2019-10-28 18:38:33 +0900

    fork 함수 사용하기
    출처:한빛미디어 유닉스 시스템 프로그래밍 6장 예제 2번

---
+++ ex6_2.c
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlin.h>
+#include <stdio.h>
+
+ int main(void) {
+     pid_t pid;
+     
+     switch (pid = fork()) {
+        case -1: /*fork failed */
+           perror("fork");
+           exit(1);
+           break;
+        case 0: /*child process*/
+        pritnf("Child Process - My PID:%d, My Parent's PID:%d\n",
+               (int)getpid(), (int)getppid());
+           break;
+           
+        default: /*parent process*/
+        printf("Parent process - My PID:%d, My Parent's PID:%d\n",
+                (int)getpid(), (int)getppid() , (int)pid);
+           break;
+      }
+  
+     printf("End of fork\n");
+  
+     return 0;
+  }
+
+ 
