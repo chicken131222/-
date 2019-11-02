aa7244cd6eff06a9f282f0c2a5cc25106e5bb7f6
ChickenMan <39923533+chicken131222@users.noreply.github.com>
2019-11-03 02:20:03 +0900

    Update ex6_4.c

--- ex6_4.c
+++ ex6_4.c
@@ -5,7 +5,9 @@
  int main(void) {
      printf("--> Before exec function\n");
      
-     if (execlp("ls" ,"ls", "-a" , (char *)NULL) == -1) {
+     if (execlp("ls" ,"ls", "-a" , (char *)NULL) == -1) {  
+      /* 8행에  execlp 함수를 호출할 때 ls -a 명령을 실행하도록 설정 
+      인자를 포인터를 써야하므로 마지막에 NULL을 지정 */
          perror("execlp");
          exit(1);
      }
@@ -14,3 +16,5 @@
      
      return 0;
   }
+
+
